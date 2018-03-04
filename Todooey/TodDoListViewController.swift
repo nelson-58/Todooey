be734e662cf7d6e3361f269fc1607b1bebdb8b4e
Andy Harris <andyharris@andys-macbook-pro.local>
2018-03-04 10:41:06 +0000

    Basic table view. Added basic datasource and delegate methods. Check mark added. Known issue: all items are checked on start up

---
+++ Todooey/TodDoListViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Todooey
+//
+//  Created by Andy Harris on 04/03/2018.
+//  Copyright © 2018 Andy Harris. All rights reserved.
+//
+
+import UIKit
+
+class TodDoListViewController : UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy eggs", "blaa bla"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // MARK - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    
+    // MARK - TableView delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print (itemArray[indexPath.row])
+        // add chck mark if there isn't one, else get rid
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        // turn off highlighting of selected cell ... looks nicer
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, performAction action: Selector, forRowAt indexPath: IndexPath, withSender sender: Any?) {
+        
+    }
+    
+    
+
+}
+
